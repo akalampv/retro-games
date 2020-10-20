76674f498ea3f83659dc3fd4da4ca1a7fe82daaa
Kalampoukas Thanasis <63159611+akalampv@users.noreply.github.com>
2020-10-20 23:55:20 +0300

    Add files via upload

---
+++ index/index.js
@@ -0,0 +1,10 @@
+/*****************
+ *               *
+ *     NAVBAR    *
+ *               *
+ * * * * * * * * */
+
+let menu = document.querySelector(".menu");
+menu.addEventListener("click", function () {
+    document.querySelector("nav").classList.toggle("responsive");
+})
